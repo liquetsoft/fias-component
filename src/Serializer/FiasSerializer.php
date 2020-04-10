3f68e2785ad4af462b11e046372bb44167c5b9ff
marvin255 <postbox433@gmail.com>
2020-04-10 19:57:48 +0200

    remove empty strings values before denormalization (#17)

--- src/Serializer/FiasSerializer.php
+++ src/Serializer/FiasSerializer.php
@@ -23,25 +23,60 @@ class FiasSerializer extends Serializer
     {
         if ($normalizers === null) {
             $normalizers = [
-                new DateTimeNormalizer,
+                new DateTimeNormalizer(),
                 new ObjectNormalizer(
                     null,
-                    new FiasNameConverter,
+                    new FiasNameConverter(),
                     null,
-                    new ReflectionExtractor,
+                    new ReflectionExtractor(),
                     null,
                     null,
-                    [ObjectNormalizer::DISABLE_TYPE_ENFORCEMENT => true]
+                    [
+                        ObjectNormalizer::DISABLE_TYPE_ENFORCEMENT => true,
+                    ]
                 ),
             ];
         }
 
         if ($encoders === null) {
             $encoders = [
-                new XmlEncoder,
+                new XmlEncoder(),
             ];
         }
 
         parent::__construct($normalizers, $encoders);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, string $type, string $format = null, array $context = [])
+    {
+        $data = $this->filterData($data);
+
+        return parent::denormalize($data, $type, $format, $context);
+    }
+
+    /**
+     * Removes items from with empty string in value from array.
+     *
+     * @param mixed $data
+     *
+     * @return mixed
+     */
+    private function filterData($data)
+    {
+        if (!is_array($data)) {
+            return $data;
+        }
+
+        $filteredData = [];
+        foreach ($data as $name => $value) {
+            if ($value !== '' || strpos($name, '@') !== 0) {
+                $filteredData[$name] = $value;
+            }
+        }
+
+        return $filteredData;
     }
 }
