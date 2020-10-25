4509af1a1990516977705dfcf801b245ebcd6509
marvin255 <postbox433@gmail.com>
2020-10-25 02:56:19 +0200

    tests typo fixes

--- tests/src/Pipeline/Task/VersionSetTaskTest.php
+++ tests/src/Pipeline/Task/VersionSetTaskTest.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Liquetsoft\Fias\Component\Tests\Pipeline\Task;
 
+use Exception;
 use Liquetsoft\Fias\Component\FiasInformer\InformerResponse;
 use Liquetsoft\Fias\Component\Pipeline\State\State;
 use Liquetsoft\Fias\Component\Pipeline\Task\Task;
 use Liquetsoft\Fias\Component\Pipeline\Task\VersionSetTask;
 use Liquetsoft\Fias\Component\Tests\BaseCase;
 use Liquetsoft\Fias\Component\VersionManager\VersionManager;
+use RuntimeException;
 
 /**
  * Тест для задачи, которая сохраняет текущую версию ФИАС.
@@ -18,6 +20,7 @@ class VersionSetTaskTest extends BaseCase
 {
     /**
      * Проверяет, что объект получает версию ФИАС и передает в менеджер версий.
+     * @throws Exception
      */
     public function testRun()
     {
@@ -30,48 +33,92 @@ class VersionSetTaskTest extends BaseCase
         $response->method('hasResult')->will($this->returnValue(true));
 
         $state = $this->getMockBuilder(State::class)->getMock();
-        $state->expects($this->once())->method('getParameter')->will($this->returnCallback(function ($name) use ($response) {
-            return $name === Task::FIAS_INFO_PARAM ? $response : null;
-        }));
+        $state->expects($this->once())
+            ->method('getParameter')
+            ->will(
+                $this->returnCallback(
+                    function ($name) use ($response) {
+                        return $name === Task::FIAS_INFO_PARAM ? $response : null;
+                    }
+                )
+            );
+        $state = $this->checkAndReturnState($state);
 
         $versionManager = $this->getMockBuilder(VersionManager::class)->getMock();
-        $versionManager->expects($this->once())->method('setCurrentVersion')->with($this->equalTo($response));
+        $versionManager->expects($this->once())
+            ->method('setCurrentVersion')
+            ->with(
+                $this->equalTo($response)
+            );
+        $versionManager = $this->checkAndReturnVersionManager($versionManager);
 
         $task = new VersionSetTask($versionManager);
+
         $task->run($state);
     }
 
     /**
      * Проверяет, что объект ничего не запишет, если результата в ответе нет.
+     * @throws Exception
      */
     public function testRunNoResult()
     {
         $response = $this->getMockBuilder(InformerResponse::class)->getMock();
-        $response->method('hasResult')->will($this->returnValue(false));
+        $response->method('hasResult')
+            ->willReturn(false);
 
         $state = $this->getMockBuilder(State::class)->getMock();
-        $state->expects($this->once())->method('getParameter')->will($this->returnCallback(function ($name) use ($response) {
-            return $name === Task::FIAS_INFO_PARAM ? $response : null;
-        }));
+        $state->expects($this->once())
+            ->method('getParameter')
+            ->will(
+                $this->returnCallback(
+                    function ($name) use ($response) {
+                        return $name === Task::FIAS_INFO_PARAM ? $response : null;
+                    }
+                )
+            );
+        $state = $this->checkAndReturnState($state);
 
         $versionManager = $this->getMockBuilder(VersionManager::class)->getMock();
         $versionManager->expects($this->never())->method('setCurrentVersion');
+        $versionManager = $this->checkAndReturnVersionManager($versionManager);
 
         $task = new VersionSetTask($versionManager);
+
         $task->run($state);
     }
 
     /**
      * Проверяет, что объект ничего не запишет, если параметра с результатом нет.
+     * @throws Exception
      */
     public function testRunNoResultParameter()
     {
         $state = $this->getMockBuilder(State::class)->getMock();
+        $state = $this->checkAndReturnState($state);
 
         $versionManager = $this->getMockBuilder(VersionManager::class)->getMock();
         $versionManager->expects($this->never())->method('setCurrentVersion');
+        $versionManager = $this->checkAndReturnVersionManager($versionManager);
 
         $task = new VersionSetTask($versionManager);
+
         $task->run($state);
+    }
+
+    /**
+     * Проверяет, что мок реализует интерфейс объекта для управления версиями.
+     *
+     * @param mixed $versionManager
+     *
+     * @return VersionManager
+     */
+    protected function checkAndReturnVersionManager($versionManager): VersionManager
+    {
+        if (!($versionManager instanceof VersionManager)) {
+            throw new RuntimeException('Wrong version manager mock.');
+        }
+
+        return $versionManager;
     }
 }
