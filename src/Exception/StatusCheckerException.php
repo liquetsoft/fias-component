473e6838b6772cc267c8373255f7e0c1c1fdcd0f
marvin255 <postbox433@gmail.com>
2020-11-14 11:50:30 +0100

    add fias status checker

---
+++ src/Exception/StatusCheckerException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Component\Exception;
+
+/**
+ * Исключение, которое выбрасывается, если не удается определить статус ФИАС.
+ */
+class StatusCheckerException extends Exception
+{
+}
---
+++ src/FiasStatusChecker/StatusCheckerResult.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Component\FiasStatusChecker;
+
+/**
+ * Объект, который содержит информацию о проверке статуса.
+ */
+class StatusCheckerResult
+{
+    /**
+     * @var string
+     */
+    private $resultStatus;
+
+    /**
+     * @var array<int, array>
+     */
+    private $perServiceStatuses;
+
+    /**
+     * @param string            $resultStatus
+     * @param array<int, array> $perServiceStatuses
+     */
+    public function __construct(string $resultStatus, array $perServiceStatuses)
+    {
+        $this->resultStatus = $resultStatus;
+        $this->perServiceStatuses = $perServiceStatuses;
+    }
+
+    /**
+     * Возвращает сттатус общего сосотояния ФИАС.
+     *
+     * @return string
+     */
+    public function getResultStatus(): string
+    {
+        return $this->resultStatus;
+    }
+
+    /**
+     * Возвращает массив со статусами по каждому сервису.
+     *
+     * @return array<int, array>
+     */
+    public function getPerServiceStatuses(): array
+    {
+        return $this->perServiceStatuses;
+    }
+}
