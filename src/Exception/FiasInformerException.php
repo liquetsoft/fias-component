5e0d6152e6978dc6d9073dc1e1971514e2d5e865
marvin255 <postbox433@gmail.com>
2021-04-09 13:58:36 +0200

    add readable exceptions for fias informer

---
+++ src/Exception/FiasInformerException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Component\Exception;
+
+/**
+ * Исключение, которое выбрасывается в случае возникновения ошибки
+ * в работе сервиса информирования ФИАС.
+ */
+class FiasInformerException extends Exception
+{
+}
