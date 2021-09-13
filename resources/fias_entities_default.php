cef0a32d2d2e622ba4575d9a434025f56dad747c
Your Name <you@example.com>
2021-09-13 02:09:15 +0200

    add correct primary for CHANGE_HISTORY

--- resources/fias_entities_default.php
+++ resources/fias_entities_default.php
@@ -3,4 +3,11 @@
 declare(strict_types=1);
 
 return [
+    'CHANGE_HISTORY' => [
+        'fields' => [
+            'CHANGEID' => [
+                'isPrimary' => true,
+            ],
+        ],
+    ],
 ];
