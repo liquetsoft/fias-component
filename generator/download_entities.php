b14e192e01fb9a8881f5f813fd898e297cdd07a4
marvin255 <postbox433@gmail.com>
2024-09-01 14:54:25 +0200

    Update XSD files

--- generator/download_entities.php
+++ generator/download_entities.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Liquetsoft\Fias\Component\Downloader\CurlDownloader;
+use Liquetsoft\Fias\Component\Downloader\DownloaderImpl;
 use Liquetsoft\Fias\Component\Helper\PathHelper;
+use Liquetsoft\Fias\Component\HttpTransport\HttpTransportCurl;
 use Liquetsoft\Fias\Component\Unpacker\ZipUnpacker;
 use Marvin255\FileSystemHelper\FileSystemFactory;
 
@@ -12,7 +14,8 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $xsdUrl = 'https://fias.nalog.ru/docs/gar_schemas.zip';
 
 $fs = FileSystemFactory::create();
-$downloader = new CurlDownloader();
+$httpTransport = new HttpTransportCurl();
+$downloader = new DownloaderImpl($httpTransport);
 $unpack = new ZipUnpacker();
 $sysTmp = __DIR__;
 $tmpFile = new SplFileInfo("{$sysTmp}/archive");
