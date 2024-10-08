89bfbf91bcb06bfdd0275a62bd7b24899238704b
liquetsoft <liquetsoft@protonmail.com>
2024-10-08 14:11:56 +0200

    Re-work SelectFilesToProceedTask

--- src/Pipeline/Task/SelectFilesToProceedTask.php
+++ src/Pipeline/Task/SelectFilesToProceedTask.php
@@ -4,25 +4,22 @@ declare(strict_types=1);
 
 namespace Liquetsoft\Fias\Component\Pipeline\Task;
 
-use Liquetsoft\Fias\Component\EntityManager\EntityManager;
 use Liquetsoft\Fias\Component\Exception\TaskException;
-use Liquetsoft\Fias\Component\Filter\Filter;
+use Liquetsoft\Fias\Component\FiasFileSelector\FiasFileSelector;
 use Liquetsoft\Fias\Component\Pipeline\State\State;
 use Liquetsoft\Fias\Component\Pipeline\State\StateParameter;
 use Psr\Log\LogLevel;
 
 /**
- * Задание, которое проверяет все файлы в распакованном архиве ФИАС
+ * Задание, которое проверяет все файлы в архиве ФИАС
  * и выбирает только те, которые можно обработать.
  */
 final class SelectFilesToProceedTask implements LoggableTask, Task
 {
     use LoggableTaskTrait;
 
-    public function __construct(
-        private readonly EntityManager $entityManager,
-        private readonly ?Filter $filter = null,
-    ) {
+    public function __construct(private readonly FiasFileSelector $fiasFileSelector)
+    {
     }
 
     /**
@@ -30,99 +27,32 @@ final class SelectFilesToProceedTask implements LoggableTask, Task
      */
     public function run(State $state): State
     {
-        $extractToFolderPath = $state->getParameterString(StateParameter::PATH_TO_EXTRACT_FOLDER);
-        $extractToFolder = $this->checkDirectory($extractToFolderPath);
+        $pathToArchive = $state->getParameterString(StateParameter::PATH_TO_DOWNLOAD_FILE);
+        $archive = new \SplFileInfo($pathToArchive);
+
+        if ($pathToArchive === '' || !$archive->isFile()) {
+            throw TaskException::create("'%s' is not a file", $pathToArchive);
+        }
 
         $this->log(
             LogLevel::INFO,
-            "Searching for files to proceed in '{$extractToFolder->getRealPath()}' folder"
+            'Selecting files from archive',
+            [
+                'path' => $pathToArchive,
+            ]
         );
 
-        $files = $this->getFilesForProceedFromFolder($extractToFolder);
+        $files = $this->fiasFileSelector->selectFiles($archive);
 
         $this->log(
             LogLevel::INFO,
-            'Found ' . \count($files) . ' file(s) to proceed',
+            'Files from archive selected',
             [
-                'files' => $files,
+                'path' => $pathToArchive,
+                'files' => \count($files),
             ]
         );
 
         return $state->setParameter(StateParameter::FILES_TO_PROCEED, $files);
-    }
-
-    /**
-     * Проверяет, что параметр директории для поиска файлов задан верно.
-     *
-     * @throws TaskException
-     */
-    private function checkDirectory(string $path): \SplFileInfo
-    {
-        $dir = new \SplFileInfo($path);
-
-        if (!$dir->isDir()) {
-            throw new TaskException(
-                "Path '{$dir->getPathname()}' must be an existed directory"
-            );
-        }
-
-        return $dir;
-    }
-
-    /**
-     * Возвращает список файлов для обработки из указанной директории.
-     *
-     * @return string[]
-     */
-    private function getFilesForProceedFromFolder(\SplFileInfo $filesFolder): array
-    {
-        $files = [];
-
-        $directoryIterator = new \RecursiveDirectoryIterator(
-            $filesFolder->getRealPath(),
-            \RecursiveDirectoryIterator::SKIP_DOTS
-        );
-        /** @var iterable<\SplFileInfo> */
-        $iterator = new \RecursiveIteratorIterator($directoryIterator);
-
-        foreach ($iterator as $fileInfo) {
-            if ($this->isFileAllowed($fileInfo)) {
-                $files[] = (string) $fileInfo->getRealPath();
-            }
-        }
-
-        sort($files, \SORT_STRING);
-
-        return $files;
-    }
-
-    /**
-     * Проверяет следует ли добавлять файл к списку.
-     */
-    private function isFileAllowed(\SplFileInfo $fileInfo): bool
-    {
-        return ($this->filter === null || $this->filter->test($fileInfo))
-            && ($this->isFileAllowedToInsert($fileInfo) || $this->isFileAllowedToDelete($fileInfo))
-        ;
-    }
-
-    /**
-     * Проверяет нужно ли файл обрабатывать для создания и обновления в рамках данного процесса.
-     */
-    private function isFileAllowedToInsert(\SplFileInfo $fileInfo): bool
-    {
-        $descriptor = $this->entityManager->getDescriptorByInsertFile($fileInfo->getFilename());
-
-        return $descriptor !== null && $this->entityManager->getClassByDescriptor($descriptor) !== null;
-    }
-
-    /**
-     * Проверяет нужно ли файл обрабатывать для удаления в рамках данного процесса.
-     */
-    private function isFileAllowedToDelete(\SplFileInfo $fileInfo): bool
-    {
-        $descriptor = $this->entityManager->getDescriptorByDeleteFile($fileInfo->getFilename());
-
-        return $descriptor !== null && $this->entityManager->getClassByDescriptor($descriptor) !== null;
     }
 }
--- tests/src/Pipeline/Task/SelectFilesToProceedTaskTest.php
+++ tests/src/Pipeline/Task/SelectFilesToProceedTaskTest.php
@@ -4,49 +4,30 @@ declare(strict_types=1);
 
 namespace Liquetsoft\Fias\Component\Tests\Pipeline\Task;
 
-use Liquetsoft\Fias\Component\EntityDescriptor\EntityDescriptor;
-use Liquetsoft\Fias\Component\EntityManager\EntityManager;
 use Liquetsoft\Fias\Component\Exception\TaskException;
-use Liquetsoft\Fias\Component\Filter\Filter;
+use Liquetsoft\Fias\Component\FiasFileSelector\FiasFileSelector;
 use Liquetsoft\Fias\Component\Pipeline\State\StateParameter;
 use Liquetsoft\Fias\Component\Pipeline\Task\SelectFilesToProceedTask;
 use Liquetsoft\Fias\Component\Tests\BaseCase;
+use Liquetsoft\Fias\Component\Unpacker\UnpackerFile;
 
 /**
- * Тест для задачи, которая выбирает файлы из папки для загрузки в базу на основе данных из EntityManager.
+ * Тест для задания, которое проверяет все файлы в архиве ФИАС
+ * и выбирает только те, которые можно обработать.
  *
  * @internal
  */
 final class SelectFilesToProceedTaskTest extends BaseCase
 {
     /**
-     * Проверяет, что объект выбросит исключение, если не найдет параметр с папкой, в которую распакованные файлы.
+     * Проверяет, что объект выбросит исключение, если файл архива не существует.
      */
-    public function testRunEmptyUnpackToException(): void
+    public function testRunUnexistedFileException(): void
     {
-        $entityManager = $this->mock(EntityManager::class);
+        $selector = $this->mock(FiasFileSelector::class);
         $state = $this->createStateMock();
 
-        $task = new SelectFilesToProceedTask($entityManager);
-
-        $this->expectException(TaskException::class);
-        $task->run($state);
-    }
-
-    /**
-     * Проверяет, что объект выбросит исключение, если апка, в которую должны быть распакованы файлы не существует.
-     */
-    public function testRunNonExitedUnpackToException(): void
-    {
-        $entityManager = $this->mock(EntityManager::class);
-
-        $state = $this->createStateMock(
-            [
-                StateParameter::PATH_TO_EXTRACT_FOLDER->value => __DIR__ . '/test',
-            ]
-        );
-
-        $task = new SelectFilesToProceedTask($entityManager);
+        $task = new SelectFilesToProceedTask($selector);
 
         $this->expectException(TaskException::class);
         $task->run($state);
@@ -57,103 +38,33 @@ final class SelectFilesToProceedTaskTest extends BaseCase
      */
     public function testRun(): void
     {
-        $fixturesFolder = __DIR__ . '/_fixtures';
+        $pathToArchive = __DIR__ . '/_fixtures/SelectFilesToProceedTaskTest/testRun.zip';
 
-        $descriptor = $this->getMockBuilder(EntityDescriptor::class)->getMock();
+        $expectedFiles = [
+            $this->mock(UnpackerFile::class),
+            $this->mock(UnpackerFile::class),
+        ];
 
-        $entityManager = $this->mock(EntityManager::class);
-        $entityManager->expects($this->any())
-            ->method('getDescriptorByInsertFile')
-            ->willReturnMap(
-                [
-                    ['SelectFilesToProceedTaskTest_insert.xml', $descriptor],
-                    ['SelectFilesToProceedTaskTest_nested_insert.xml', $descriptor],
-                ]
-            );
-        $entityManager->expects($this->any())
-            ->method('getDescriptorByDeleteFile')
-            ->willReturnMap(
-                [
-                    ['SelectFilesToProceedTaskTest_delete.xml', $descriptor],
-                    ['SelectFilesToProceedTaskTest_nested_delete.xml', $descriptor],
-                ]
-            );
-
-        $entityManager->expects($this->any())
-            ->method('getClassByDescriptor')
-            ->willReturnMap(
-                [
-                    [$descriptor, \stdClass::class],
-                ]
-            );
+        $selector = $this->mock(FiasFileSelector::class);
+        $selector->expects($this->once())
+            ->method('selectFiles')
+            ->with(
+                $this->callback(
+                    fn (\SplFileInfo $t): bool => $t->getRealPath() === $pathToArchive
+                )
+            )
+            ->willReturn($expectedFiles);
 
         $state = $this->createStateMock(
             [
-                StateParameter::PATH_TO_EXTRACT_FOLDER->value => $fixturesFolder,
+                StateParameter::PATH_TO_DOWNLOAD_FILE->value => $pathToArchive,
             ]
         );
 
-        $task = new SelectFilesToProceedTask($entityManager);
+        $task = new SelectFilesToProceedTask($selector);
         $newState = $task->run($state);
+        $res = $newState->getParameter(StateParameter::FILES_TO_PROCEED);
 
-        $this->assertSame(
-            [
-                $fixturesFolder . '/SelectFilesToProceedTaskTest_delete.xml',
-                $fixturesFolder . '/SelectFilesToProceedTaskTest_insert.xml',
-                $fixturesFolder . '/nested/SelectFilesToProceedTaskTest_nested_delete.xml',
-                $fixturesFolder . '/nested/SelectFilesToProceedTaskTest_nested_insert.xml',
-            ],
-            $newState->getParameter(StateParameter::FILES_TO_PROCEED)
-        );
-    }
-
-    /**
-     * Проверяет, что объект правильно получит список файлов для обработки с использованием фильтра.
-     */
-    public function testRunWithFilter(): void
-    {
-        $fixturesFolder = __DIR__ . '/_fixtures';
-
-        $descriptor = $this->mock(EntityDescriptor::class);
-
-        $entityManager = $this->mock(EntityManager::class);
-        $entityManager->expects($this->any())
-            ->method('getDescriptorByInsertFile')->willReturnMap(
-                [
-                    ['SelectFilesToProceedTaskTest_insert.xml', $descriptor],
-                    ['SelectFilesToProceedTaskTest_nested_insert.xml', $descriptor],
-                ]
-            );
-        $entityManager->expects($this->any())
-            ->method('getClassByDescriptor')->willReturnMap(
-                [
-                    [$descriptor, \stdClass::class],
-                ]
-            );
-
-        $filter = $this->mock(Filter::class);
-        $filter->expects($this->any())
-            ->method('test')
-            ->willReturnCallback(
-                function (\SplFileInfo $file) use ($fixturesFolder) {
-                    return ((string) $file) === $fixturesFolder . '/nested/SelectFilesToProceedTaskTest_nested_insert.xml';
-                }
-            );
-
-        $state = $this->createStateMock(
-            [
-                StateParameter::PATH_TO_EXTRACT_FOLDER->value => $fixturesFolder,
-            ]
-        );
-
-        $task = new SelectFilesToProceedTask($entityManager, $filter);
-        $newState = $task->run($state);
-
-        $this->assertSame(
-            [
-                $fixturesFolder . '/nested/SelectFilesToProceedTaskTest_nested_insert.xml',
-            ],
-            $newState->getParameter(StateParameter::FILES_TO_PROCEED)
-        );
+        $this->assertSame($expectedFiles, $res);
     }
 }
